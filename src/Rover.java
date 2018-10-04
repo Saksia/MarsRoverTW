d69ee845e91be5c889de953fdc0e4cd2421ed192
Saskia <saskia.santana101@alu.ulpgc.es>
2018-10-04 21:13:49 +0100

    New class 'Plateau' and exceptions added.

--- src/Rover.java
+++ src/Rover.java
@@ -2,26 +2,64 @@ public class Rover {
 
     private int x;
     private int y;
+    private Plateau plateau;
     Orientation orientation;
-    String coordinates;
-    
-    private int plateauX = 5;
-    private int plateauY = 5;
+
     
     
-    public Rover(int x, int y, Orientation orientation){
+    public Rover(int x, int y, Orientation orientation, Plateau plateau){
         this.x = x;
         this.y = y;
         this.orientation = orientation;
-        coordinates = x + " " + y;
+        this.plateau = plateau;
     }
 
-     String changeDirection(String command){
-        for (char c: command.toCharArray()) {
+     public String executeCommand(String command){
+
+        for (char c : command.toCharArray()) {
             if(c == 'L') orientation = orientation.left();
-            if(c == 'R') orientation = orientation.right();
+            else if(c == 'R') orientation = orientation.right();
+            else if(c == 'M') move();
+            else throw new AssertionError("ERROR. Commands should be 'L', 'R' or 'M'");
         }
-        return coordinates + " " + orientation.value();
+
+        return setCoordinates();
+    }
+
+    private String setCoordinates() {
+        return this.x + " " + this.y + " " + orientation.value();
+    }
+
+    private void move() {
+
+        switch (orientation.value()){
+            case "N":
+                checkIfNewPositionIsLegal(this.x, this.y++);
+                this.y++;
+                break;
+            case "S":
+                checkIfNewPositionIsLegal(this.x, this.y--);
+                this.y--;
+                break;
+            case "E":
+                checkIfNewPositionIsLegal(this.x++, this.y);
+                this.x++;
+                break;
+            case "W":
+                checkIfNewPositionIsLegal(this.x--, this.y);
+                this.x--;
+                break;
+            default:
+                throw new AssertionError("ERROR. Orientation of the rover should be 'N', 'S', 'E' or 'W'");
+        }
+    }
+
+    private void checkIfNewPositionIsLegal(int newPositionX, int newPositionY){
+        if((newPositionX > plateau.getUpperRightX()
+                || newPositionY > plateau.getUpperRightY())
+                || (newPositionX < 0 || newPositionY < 0 )){
+            throw new AssertionError("ERROR. The rover cannot be outside the plateau");
+        }
     }
 
 
--- tests/Rover_Direction_.java
+++ tests/Rover_Direction_.java
@@ -6,48 +6,64 @@ import static org.junit.Assert.*;
 
 public class Rover_Direction_ {
 
-    private Rover rover;
+    private Plateau plateau;
+    private Rover rover, rover2;
 
     @Before
     public void setUp(){
-        rover = new Rover(1,3, Orientation.NORTH);
+
+        plateau = new Plateau(5,5);
+        rover = new Rover(1,3, Orientation.NORTH, plateau);
+
     }
 
 
     //RIGHT
     @Test
-    public void should_be_East_when_rotate_right(){
-        assertThat(rover.changeDirection("R"), is("1 3 E"));
+    public void should_be_East_when_rotate_right() throws Exception{
+        assertThat(rover.executeCommand("R"), is("1 3 E"));
     }
     @Test
-    public void should_be_South_when_rotate_right(){
-        assertThat(rover.changeDirection("RR"), is("1 3 S"));
+    public void should_be_South_when_rotate_right() throws Exception{
+        assertThat(rover.executeCommand("RR"), is("1 3 S"));
     }
     @Test
-    public void should_be_West_when_rotate_right(){
-        assertThat(rover.changeDirection("RRR"), is("1 3 W"));
+    public void should_be_West_when_rotate_right() throws Exception{
+        assertThat(rover.executeCommand("RRR"), is("1 3 W"));
     }
     @Test
-    public void should_be_North_when_rotate_right(){
-        assertThat(rover.changeDirection("RRRR"), is("1 3 N"));
+    public void should_be_North_when_rotate_right() throws Exception{
+        assertThat(rover.executeCommand("RRRR"), is("1 3 N"));
     }
 
 
     //LEFT
     @Test
-    public void should_be_West_when_rotate_left(){
-        assertThat(rover.changeDirection("L"), is("1 3 W"));
+    public void should_be_West_when_rotate_left() throws Exception{
+        assertThat(rover.executeCommand("L"), is("1 3 W"));
     }
     @Test
-    public void should_be_South_when_rotate_left(){
-        assertThat(rover.changeDirection("LL"), is("1 3 S"));
+    public void should_be_South_when_rotate_left() throws Exception{
+        assertThat(rover.executeCommand("LL"), is("1 3 S"));
     }
     @Test
-    public void should_be_East_when_rotate_left(){
-        assertThat(rover.changeDirection("LLL"), is("1 3 E"));
+    public void should_be_East_when_rotate_left() throws Exception{
+        assertThat(rover.executeCommand("LLL"), is("1 3 E"));
     }
     @Test
-    public void should_be_North_when_rotate_left(){
-        assertThat(rover.changeDirection("LLLL"), is("1 3 N"));
+    public void should_be_North_when_rotate_left() throws Exception{
+        assertThat(rover.executeCommand("LLLL"), is("1 3 N"));
+    }
+
+
+    @Test (expected = AssertionError.class)
+    public void should_return_exception_message() throws Exception{
+        rover.executeCommand("LLRRLH");
+    }
+
+
+    //Running a few more tests for acceptance.
+    @Test
+    public void acceptance() throws Exception{
     }
 }
